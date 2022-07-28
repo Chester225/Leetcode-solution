42a49b924475549368e43c0803144d8955a63041
Chester225 <85096117+Chester225@users.noreply.github.com>
2022-07-28 09:50:47 +0800

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,3 +1,6 @@
+# Runtime: 55 ms, faster than 99.26% of Python3 online submissions for Two Sum.
+# Memory Usage: 15.2 MB, less than 50.46% of Python3 online submissions for Two Sum.
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         preMap={}
